4079f98d5ef3d4e92a633b9ecf510b95d2db1abf
John R. Wolfe <johnwolfe@mailetechnical.com>
2022-08-21 14:46:48 -0700

    Works as expected

--- Tests/run.sh
+++ Tests/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp $HOME/.m2/repository/io/ciera/runtime/2.4.1-SNAPSHOT/runtime-2.4.1-SNAPSHOT.jar:$HOME/.m2/repository/org/antlr/antlr4-runtime/4.9.1/antlr4-runtime-4.9.1.jar:$HOME/.m2/repository/org/xtuml/Tests/1.0.0-SNAPSHOT/Tests-1.0.0-SNAPSHOT.jar tests.TestsApplication
+java -cp $HOME/.m2/repository/io/ciera/runtime/2.5.4/runtime-2.5.4.jar:$HOME/.m2/repository/org/antlr/antlr4-runtime/4.9.1/antlr4-runtime-4.9.1.jar:$HOME/.m2/repository/org/xtuml/AdHocTests/1.0.0-SNAPSHOT/AdHocTests-1.0.0-SNAPSHOT.jar tests.TestsApplication
